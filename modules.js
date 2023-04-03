a9eb5f84be331b11d6e84bf09d31f5ce3158a000
Siegfred Samson <siegfredsamson@gmail.com>
2023-04-03 09:48:28 +0100

    exporting multiple module items

--- modules.js
+++ modules.js
@@ -1,13 +1,17 @@
 // to import people from people.js
-const xyz = require("./people");
+const xyz = require("./peoples");
 
 // running clg indicates that xyz however is an empty object
-console.log(xyz); // returns an empty array
+// console.log(xyz); // returns an empty array
+
+// to access multiple items
+console.log(xyz.people, xyz.ages); // returns an empty array
 
 // running people indicates that xyz however is an empty object
 // Just because we import a file doesn't automatically grant us access to it's contents
 
-// console.log(people); // returns undefined
+// console.log(peoples); // returns undefined
 
 // to access the contents of people, we have to manually export
 // to do this, we use module.export = 'anything' in the parent module
+// this is equal to xyz in modules.js
--- peoples.js
+++ peoples.js
@@ -1,5 +1,16 @@
 const people = ["Sam", "Jason", "Rachael", "Isaac"];
+const ages = [3, 6, 7, 4];
 
 console.log(people);
 
 module.exports = "type_anything";
+// It doesnt matter what name you use for module.exports, it will equate what you import with require
+
+// exporting multiple items
+module.exports = {
+  // people: people,
+  // ages: ages
+  // or simply
+  people,
+  ages,
+};
