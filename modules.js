2c9a611cdc824a80e1eba9583fb356233540a7b9
Siegfred Samson <siegfredsamson@gmail.com>
2023-04-03 11:02:57 +0100

    importing nodejs built-in modukles

--- modules.js
+++ modules.js
@@ -4,9 +4,6 @@ const xyz = require("./peoples");
 // running clg indicates that xyz however is an empty object
 // console.log(xyz); // returns an empty array
 
-// to access multiple items
-console.log(xyz.people, xyz.ages); // returns an empty array
-
 // running people indicates that xyz however is an empty object
 // Just because we import a file doesn't automatically grant us access to it's contents
 
@@ -15,3 +12,15 @@ console.log(xyz.people, xyz.ages); // returns an empty array
 // to access the contents of people, we have to manually export
 // to do this, we use module.export = 'anything' in the parent module
 // this is equal to xyz in modules.js
+
+// ACCESSING MULTIPLE ITEMS OF A MODULE
+console.log(xyz.people, xyz.ages);
+
+// TO IMPORT MULTIPLE ITEMS USING DESTRUCTURING
+// replace const xyz = require("./peoples"); with
+const { people, ages } = require("./peoples");
+console.log(people, ages);
+
+// IMPORTING BUILT-IN NODEJS MODULES FOR ADDED FUNCTIONALIY
+const os = require("os");
+console.log(os);
