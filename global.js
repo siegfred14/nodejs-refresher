58fb1d8f2f8669e484ab10b767c4065b1494a217
Siegfred Samson <siegfredsamson@gmail.com>
2023-04-03 07:56:43 +0100

    using setTimeout and setInterval

--- global.js
+++ global.js
@@ -2,8 +2,18 @@
 
 // in JS, the global object is window, but in nodejs, it is global
 
-console.log(global);
+// console.log(global);
 
-global.setTimeout(() => {
+// global.setTimeout(() => {
+//   console.log("testing the timeout");
+// }, 3000);
+
+// so just like in js where we dont have to add window to the browser functions, we dont need to add "global" either in nodejs
+
+setTimeout(() => {
   console.log("testing the timeout");
 }, 3000);
+
+const int = setInterval(() => {
+  console.log("testing the interval");
+}, 1000);
