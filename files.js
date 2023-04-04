1c4cf917929c9c1eb66634421b7be3423d56e671
Siegfred Samson <siegfredsamson@gmail.com>
2023-04-04 08:13:39 +0100

    working with the nodejs file system.js

---
+++ files.js
@@ -0,0 +1,10 @@
+// WORKING WITH FILE SYSTEM CORE MODULE
+// The file system core module allows us to perform CRUD operations in nodejs
+
+// Importing the file system core module
+const fs = require("fs");
+
+// Reading files
+// Writing files
+// directories
+// deleting files
