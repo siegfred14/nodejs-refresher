6cd83d976030edb126040990a73216e188e35368
Siegfred Samson <siegfredsamson@gmail.com>
2023-04-04 08:52:38 +0100

    writing files

--- files.js
+++ files.js
@@ -21,5 +21,18 @@ fs.readFile("./docs/test_file.txt", (err, data) => {
 console.log("last line of code");
 
 // WRITING FILES
+// Takes in 3 arguments
+// 1. the string for the file location to be read
+// 2. the data/text you want to write. this replaces the existing text
+// 3. an assynchronous function
+fs.writeFile("./docs/test_file.txt", "Salut Lavaloise et Lavalois", () => {
+  console.log("update written");
+});
+
+// if we do this with a file that does not exist, it will just create the new file
+fs.writeFile("./docs/test_file2.txt", "Salut Quebecoise et Quebecois", () => {
+  console.log("update written");
+});
+
 // DIRECTORIES
 // DELETING FILES
