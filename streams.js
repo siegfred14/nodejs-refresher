4b0131b7a866508e9e1276ff56e9bc2789171f91
Siegfred Samson <siegfredsamson@gmail.com>
2023-04-05 14:47:02 +0100

    initial streams

---
+++ streams.js
@@ -0,0 +1,7 @@
+// Working with Large files
+// Streams - start using date before it has finished loading
+// buffer - little chunks of data, that are sent one at a time, which user can
+// use, before the entirety is received. eg video on youtube
+
+// types include read streams and write streams
+const fs = require("fs");
