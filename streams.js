ebd25ed8668ff59a7226b5a1dfccb1b972edd3f5
Siegfred Samson <siegfredsamson@gmail.com>
2023-04-05 15:32:33 +0100

    converting data to utf8

--- streams.js
+++ streams.js
@@ -9,17 +9,22 @@ const { Stream } = require("stream");
 
 // to read from Stream.txt
 
-const readStream = fs.createReadStream("./docs/streams.txt"); // create the ream stream
+// const readStream = fs.createReadStream("./docs/streams.txt"); // create the read stream
+const readStream = fs.createReadStream("./docs/streams.txt", {
+  encoding: "utf8",
+});
 
 readStream.on("data", (chunk) => {
   console.log(`.....new chunk.... \n`);
-  console.log(chunk);
+  console.log(chunk); // note that "chunk" could be any name
 });
 
 // .on is an event listener
 
 // to convert streams to string, we use
-readStream.on("data", (chunk) => {
-  console.log(`.....new chunk.... \n`);
-  console.log(chunk.toString());
-});
+// readStream.on("data", (chunk) => {
+//   console.log(`.....new chunk.... \n`);
+//   console.log(chunk.toString());
+// });
+
+// alternative method of conversion to string is to add encoding type on line 12
