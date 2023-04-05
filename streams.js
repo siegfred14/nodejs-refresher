455875cc015698de85e0dfd5c9d7ff2a7b5f2df5
Siegfred Samson <siegfredsamson@gmail.com>
2023-04-05 15:13:10 +0100

    reading large files in chunks

--- streams.js
+++ streams.js
@@ -5,3 +5,21 @@
 
 // types include read streams and write streams
 const fs = require("fs");
+const { Stream } = require("stream");
+
+// to read from Stream.txt
+
+const readStream = fs.createReadStream("./docs/streams.txt"); // create the ream stream
+
+readStream.on("data", (chunk) => {
+  console.log(`.....new chunk.... \n`);
+  console.log(chunk);
+});
+
+// .on is an event listener
+
+// to convert streams to string, we use
+readStream.on("data", (chunk) => {
+  console.log(`.....new chunk.... \n`);
+  console.log(chunk.toString());
+});
