570ab4f81ffa2cbf0f1465141737666753346173
Siegfred Samson <siegfredsamson@gmail.com>
2023-04-06 15:55:31 +0100

    initial clients and servers: servers

---
+++ server.js
@@ -0,0 +1,3 @@
+// In  node, we write code to create a server and listen for requests coming in from the browser
+// Unlike php which has programs that automatically do this like apache, in node, it is created manually
+// to create a server, we can name the file anything, not necessarily server.js
