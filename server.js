7e00721ab2d62e85d1f54250230ed19ccb8e7522
Siegfred Samson <siegfredsamson@gmail.com>
2023-04-11 07:21:08 +0100

    package.json for project specific packages

--- server.js
+++ server.js
@@ -165,6 +165,7 @@ const server = http.createServer((req, res) => {
     case "/about-me":
       res.statusCode = 301;
       res.setHeader("Location", "/about");
+      res.end();
       break;
     case "/contact":
       path += "contact.html";
