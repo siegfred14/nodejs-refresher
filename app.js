944342072edb83555c0076052dbedd17c102f570
Siegfred Samson <siegfredsamson@gmail.com>
2023-04-14 08:44:57 +0100

    responding with an HTML file

--- app.js
+++ app.js
@@ -11,3 +11,23 @@ app.get("/", (req, res) => {
   //This replaces res.write, and automatically detects the content type, hence no need for setHeader
   // It also automatically infers the status code (200, 301, 404)
 });
+
+// RESPONDING TO MULTIPLE HTML PAGES
+// we could repeat same process for multiple request as such
+
+// app.get("/about", (req, res) => {
+//     res.send("<p>About Page</p>");
+//   });
+// app.get("/contact", (req, res) => {
+//     res.send("<p>Contact Page</p>");
+//   });
+
+//   Or to respond with a file
+// we must specify the root, else it would search file from computer default root
+app.get("/about", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+app.get("/contact", (req, res) => {
+  res.sendFile("./views/contact.html", { root: __dirname });
+});
+//   another way is to use the core module to join the pathname with the root
