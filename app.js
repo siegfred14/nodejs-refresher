1726ec8187dcdd21299bb99f8a56d7c009810c48
Siegfred Samson <siegfredsamson@gmail.com>
2023-04-14 08:29:40 +0100

    responding to a single url with Express

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.send(<p>Home Page</p>);
+  res.send("<p>Home Page</p>");
   //This replaces res.write, and automatically detects the content type, hence no need for setHeader
   // It also automatically infers the status code (200, 301, 404)
 });
