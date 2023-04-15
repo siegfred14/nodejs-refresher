b57915fa6473c40e0b5ed667bf4526e38acfca0b
Siegfred Samson <siegfredsamson@gmail.com>
2023-04-15 05:44:21 +0100

    404 with Express

--- app.js
+++ app.js
@@ -36,3 +36,9 @@ app.get("/about", (req, res) => {
 app.get("/about-us", (req, res) => {
   res.redirect("/about");
 });
+
+// 404 page
+app.use((req, res) => {
+  res.sendFile("./views/404.html", { root: __dirname });
+});
+// We use this function to create and fire middleware in express
